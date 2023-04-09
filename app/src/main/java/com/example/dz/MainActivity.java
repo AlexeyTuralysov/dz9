9808cd40cc12809f607b4f64920adcdf45289d37
AlexeyMarkul <72796090+AlexeyMarkul@users.noreply.github.com>
2023-04-09 21:46:12 +0900

    first commit

---
+++ app/src/main/java/com/example/dz/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // создаем обработчик нажатия
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        btnOk.setOnClickListener(oclBtnOk);
+        btnCancel.setOnClickListener(oclBtnCancel);
+    }
+}
